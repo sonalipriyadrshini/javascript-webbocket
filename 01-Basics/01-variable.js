4578ae443d650481c67dd736625d80acfa89f029
sonalipriyadrshini <152278111+sonalipriyadrshini@users.noreply.github.com>
2023-11-28 12:22:05 +0000

    test file addes

---
+++ 01-Basics/01-variable.js
@@ -0,0 +1,8 @@
+/*1. Variable is a conitainer to store some data.
+  2.There are 3type of variable in javascript
+   1.let(let is used on block scope)
+   2.const
+   3.var(var is used on functional scope)
+
+ 
+ */
