7306030c7ac6d61a733baf4b6cdaad7b836e3e12
sonalipriyadrshini <152278111+sonalipriyadrshini@users.noreply.github.com>
2023-12-12 12:21:34 +0000

    hello

--- 01-Basics/object.js
+++ 01-Basics/object.js
@@ -33,3 +33,74 @@
  }
  person.greeting()
 person.greetingTwo()
+
+//object constructor
+const user={};
+user.userid=5635;
+user.username="smruti";
+user.userstatus="active";
+user.isloggedin=true;
+console.log(user)
+console.log(Object.entries(user))
+console.log(Object.keys(person))
+console.log(Object.values(person))
+console.log(person.hasOwnProperty("isLoggedin"))
+console.log(person.hasOwnProperty("gender"))
+
+//nested objects
+const userName={
+    userDetails:{
+        usserFullName:{
+            lname:"malik"
+            ,fname:"smruti"
+        }
+    }
+}
+console.log(userName)
+console.log(userName.userDetails.usserFullName.fname)
+
+//assign method(it joins the two object)
+const obj1={a:1,b:2}
+const obj2={c:3,d:5}
+const obj3={e:6,f:7}
+const obj4={obj1,obj2,obj3}
+//console.log(obj4)
+const spread={...obj1,...obj2,...obj3}
+//console.log(spread)
+const obj=Object.assign({},obj1,obj2,obj3)
+console.log(obj)
+
+//Objects in array
+const arrayObj=[
+    {
+        id:101,
+        location:"bbsr"
+    },
+    {
+        id:102,
+        location:"cuttack"
+    },
+    {
+        id:103,
+        location:"puri"
+    },
+    {
+        id:104,
+        location:"paradeep"
+    }
+]
+console.log(arrayObj);
+console.log(arrayObj[2])
+
+//object de-structuring
+const courseDetails={
+    courseName:"javascript",
+    courseFee:"4000",
+    proviedBy:"web_bocket"
+};
+//console.log(courseDetails);
+const {proviedBy:pd}=courseDetails;
+console.log(pd)
+//pb="Bocket_lamp";
+//we can only print the values by destructring technique but can't change the values of key by de structring technque
+//console.log(pb)
